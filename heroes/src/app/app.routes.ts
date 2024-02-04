8b2d81307e9b736ba60ef14bd616f8794336620c
Alexis Navarro Vazquez <alexisnavarrovazquez19@gmail.com>
2024-02-04 16:43:59 +0100

    feat(routes): creation of the app routes

--- heroes/src/app/app.routes.ts
+++ heroes/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HeroesFormComponent } from "./components/heroes-form/heroes-form.component";
+import { HeroesListComponent } from "./components/heroes-list/heroes-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HeroesListComponent },
+    { path: "heroes-form", component: HeroesFormComponent },
+];
